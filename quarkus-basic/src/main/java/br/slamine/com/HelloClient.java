45f3088480cf8fcb4c2db90062f369849df74b86
Simeão David Lamine <simeaolm@gmail.com>
2020-07-14 00:33:00 -0300

    Exemplifying fault tolerance in quarkus using rest client

---
+++ quarkus-basic/src/main/java/br/slamine/com/HelloClient.java
@@ -0,0 +1,17 @@
+package br.slamine.com;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+@RestClient
+public interface HelloClient {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello();
+}
--- quarkus-basic/src/main/java/br/slamine/com/HelloResource.java
+++ quarkus-basic/src/main/java/br/slamine/com/HelloResource.java
@@ -1,7 +1,11 @@
 package br.slamine.com;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -14,6 +18,10 @@ public class HelloResource {
     @Inject
     private GreetingService greetingService;
 
+    @Inject
+    @RestClient
+    private HelloClient helloClient;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
@@ -22,10 +30,22 @@ public class HelloResource {
                 .orElse("unknown");
     }
 
-    @GET()
+    @GET
     @Path("/async")
     @Produces(MediaType.TEXT_PLAIN)
     public CompletionStage<String> async(){
         return CompletableFuture.supplyAsync(() -> "I'm async");
+    }
+
+    @Fallback(fallbackMethod = "fallback")
+    @GET
+    @Path("/remote")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String remoteHello(){
+       return helloClient.hello();
+    }
+
+    public String fallback(){
+        return "I'm fallback";
     }
 }
