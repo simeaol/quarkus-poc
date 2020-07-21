fee9d40c5529aabc11d871eb915464a05284ed4a
Simeão David Lamine <simeaolm@gmail.com>
2020-07-20 23:07:05 -0300

    [OpenTracing-Eager] Open tracing poc using eager
    - By default all rest enpoint is tracked
    - Use annotation @Traced to track another service or flow in you project

--- quarkus-basic/src/main/java/br/slamine/com/developers/DeveloperRepository.java
+++ quarkus-basic/src/main/java/br/slamine/com/developers/DeveloperRepository.java
@@ -1,11 +1,13 @@
 package br.slamine.com.developers;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import org.eclipse.microprofile.opentracing.Traced;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
 @ApplicationScoped
+@Traced
 public class DeveloperRepository implements PanacheRepository<Developer> {
     public List<Developer> findDevs(String filter){
         return list("name like concat('%', ?1, '%')", filter);
